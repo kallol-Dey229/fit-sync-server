c573a291cb2299a2dc48350b4eaecfcbe74ef608
Kallol Dey <kalloldey067@gmail.com>
2026-06-20 20:37:21 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const app = express();
+const port = 5000;
+require('dotenv').config();
+const { MongoClient, ServerApiVersion } = require('mongodb');
+
+app.get('/', (req, res) => {
+  res.send('Server is running........!')
+})
+
+
+
+
+
+
+const uri =  process.env.MONGO_DB_URI;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    
+    await client.connect();
+    
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+
+
+
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
