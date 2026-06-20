abbf8460c534248db53f254046b7d3e0ba39289f
Kallol Dey <kalloldey067@gmail.com>
2026-06-20 20:53:42 +0600

    class add , post api created

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 const port = 5000;
 require('dotenv').config();
+
+
+app.use(cors());
+app.use(express.json());
+
+
+
 const { MongoClient, ServerApiVersion } = require('mongodb');
 
 app.get('/', (req, res) => {
-  res.send('Server is running........!')
+    res.send('Server is running........!')
 })
 
 
@@ -13,29 +21,48 @@ app.get('/', (req, res) => {
 
 
 
-const uri =  process.env.MONGO_DB_URI;
+const uri = process.env.MONGO_DB_URI;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  }
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
 });
 
 async function run() {
-  try {
-    
-    await client.connect();
-    
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } finally {
+    try {
 
-    // Ensures that the client will close when you finish/error
-    // await client.close();
-  }
+        await client.connect();
+
+
+        const database = client.db("fit-sync");
+        const ClassCollection = database.collection("classes");
+
+
+        //class api
+
+        app.post('/api/classes', (req, res) => {
+
+            const newClass = req.body;
+
+            const result = await ClassCollection.insertOne(newClass);
+            res.send(result);
+        })
+
+
+
+
+
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+
+        // Ensures that the client will close when you finish/error
+        // await client.close();
+    }
 }
 run().catch(console.dir);
 
@@ -44,5 +71,5 @@ run().catch(console.dir);
 
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+    console.log(`Example app listening on port ${port}`)
 })
